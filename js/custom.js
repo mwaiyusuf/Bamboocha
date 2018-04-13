9c59798fb8118abd269250100b965c1f9953eafc
mohammed yusuf <me@mwaigalo5.com>
2018-04-13 11:30:22 +0300

    js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,16 @@
-var x=prompt('please enter your name');
+function vans(){
+    var size=prompt("enter your shoe size");
+    if(size==6){
+        document.write(" if its " + size + ",it cost 800 ");
+    }
+    else if(size==7){
+        document.write(" if its " + size + ", it cost 1500 ");
+    }
+    else if(size==8){
+        document.write(" if its " + size + ", it cost 2000 ");
+    }
+        else{
+       document.write( size + " it will cost 2400 ");
+        }
+}
+vans();
