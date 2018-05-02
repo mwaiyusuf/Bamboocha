3eab1878c83c7087fed9aa4bfbdf46e066f4ee5b
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-02 22:04:55 +0300

    b

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,18 @@
 function vans(){
+    var name=prompt("Enter your name");
+    alert("Hi " + name);
     var size=prompt("enter your shoe size");
     if(size==6){
-        document.write(" if its " + size + ",it cost 800 ");
+        alert(" if its " + size + ",it cost 800 ");
     }
     else if(size==7){
-        document.write(" if its " + size + ", it cost 1500 ");
+        alert(" if its " + size + ", it cost 1500 ");
     }
     else if(size==8){
-        document.write(" if its " + size + ", it cost 2000 ");
+        alert(" if its " + size + ", it cost 2000 ");
     }
         else{
-       document.write( size + " it will cost 2400 ");
+       alert( size + " it will cost 2400 ");
         }
 }
 vans();
