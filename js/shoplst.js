a870a01b3e86820dcd4076a909e7e3cf19c81d60
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-15 15:27:24 +0300

    r

---
+++ js/shoplst.js
@@ -0,0 +1,16 @@
+var itemInput = document.getElementById("item");
+//   var clear = document.getElementById("clear");
+//   var submit = document.getElementById("submit");
+//   var list = document.getElementById("list");
+//   function addItem() {
+//     var currentItem = itemInput.value;
+//     var listItem = document.createElement("li");
+//     listItem.innerHTML = currentItem;
+//     list.appendChild(listItem);
+//     itemInput.value = "";
+//   }
+//   submit.onclick = addItem;
+//   clear.onclick = function() {
+//     list.innerHTML = "";
+//     itemInput.value = "";
+//   }
