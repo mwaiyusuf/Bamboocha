9056f7922f688537543425dd9b5083476c5ce45d
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-03 12:01:32 +0300

    t

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,25 @@
-function vans(){
-    var name=prompt("Enter your name");
-    alert("Hi " + name);
-    var size=prompt("enter your shoe size");
-    if(size==6){
-        alert(" if its " + size + ",it cost 800 ");
-    }
-    else if(size==7){
-        alert(" if its " + size + ", it cost 1500 ");
-    }
-    else if(size==8){
-        alert(" if its " + size + ", it cost 2000 ");
-    }
-        else{
-       alert( size + " it will cost 2400 ");
-        }
-    var Age=prompt("Enter your Age");
-    if(Age<18){
-        alert("please visit the cartoon network");
-    }
-    else{
-        alert("Welcome to this amazing site");
-    }
-}
-vans();
+//function vans(){
+//    var name=prompt("Enter your name");
+//    alert("Hi " + name);
+//    var size=prompt("enter your shoe size");
+//    if(size==6){
+//        alert(" if its " + size + ",it cost 800 ");
+//    }
+//    else if(size==7){
+//        alert(" if its " + size + ", it cost 1500 ");
+//    }
+//    else if(size==8){
+//        alert(" if its " + size + ", it cost 2000 ");
+//    }
+//        else{
+//       alert( size + " it will cost 2400 ");
+//        }
+//    var Age=prompt("Enter your Age");
+//    if(Age<18){
+//        alert("please visit the cartoon network");
+//    }
+//    else{
+//        alert("Welcome to this amazing site");
+//    }
+//}
+//vans();
--- js/shoplst.js
+++ js/shoplst.js
@@ -3,14 +3,14 @@ var itemInput = document.getElementById("item");
    var submit = document.getElementById("submit");
    var list = document.getElementById("list");
    function addItem() {
-//     var currentItem = itemInput.value;
-//     var listItem = document.createElement("li");
-//     listItem.innerHTML = currentItem;
-//     list.appendChild(listItem);
-//     itemInput.value = "";
-//   }
-//   submit.onclick = addItem;
-//   clear.onclick = function() {
-//     list.innerHTML = "";
-//     itemInput.value = "";
-//   }
+     var currentItem = itemInput.value;
+     var listItem = document.createElement("li");
+     listItem.innerHTML = currentItem;
+     list.appendChild(listItem);
+     itemInput.value = "";
+   }
+   submit.onclick = addItem;
+   clear.onclick = function() {
+     list.innerHTML = "";
+     itemInput.value = "";
+   }
