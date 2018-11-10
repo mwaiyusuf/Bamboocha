63cc993da484343d03457e1c629a53af9b9574f0
mwaiyusuf <mwaigalo5@gmail.com>
2018-11-10 08:23:20 +0300

    t

--- js/server.js
+++ js/server.js
@@ -3,8 +3,8 @@ const hostname = '127.0.0.1'
 const port = 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.end('Hey,welcome to Node.js!\n')
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hey,welcome to Node.js!\n')
 // })
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`)
