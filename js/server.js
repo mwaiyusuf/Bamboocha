244e7a94abb12472af269a8a5600a47a4fe1768d
mwaiyusuf <mwaigalo5@gmail.com>
2018-11-10 08:24:26 +0300

    G

--- js/server.js
+++ js/server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
   res.end('Hey,welcome to Node.js!\n')
-// })
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`)
-// })
+})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
